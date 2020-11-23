db472da8811b7e212218534fec6ea1b993234e77
Miriam <49313339+stilografica@users.noreply.github.com>
2020-11-23 17:49:32 +0100

    Responsive revis. y interacciones menu hamburguesa

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+
+//Hamburger
+let hamburger=document.getElementById("hamburger");
+let hamburgerCross=document.getElementById("hamburgerCross");
+let menu=document.getElementById("main-menu");
+hamburger.addEventListener("click",open);
+menu.addEventListener("click",close);
+hamburgerCross.addEventListener("click",close);
+
+function open(){
+    hamburgerCross.style.display="block";
+    hamburger.style.display="none";
+    menu.style.display="block";
+    
+}
+
+function close(){
+    menu.style.display="none";
+    hamburgerCross.style.display="none";
+    hamburger.style.display="block";
+}
